7808821361140352c5ac1dabcf6771d19daca89b
Mudit Gaur <mudit03@gmail.com>
2024-05-19 14:34:36 +0530

    adds desired caps for remote webdriver

--- src/test/java/framework/UITests/BaseTestClass.java
+++ src/test/java/framework/UITests/BaseTestClass.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class BaseTestClass {
@@ -13,9 +18,11 @@ public class BaseTestClass {
     static WebDriver driver;
 
     @BeforeAll
-    static void setup(){
+    static void setup() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName(Browser.CHROME.browserName());
 
-        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL("http://localhost:8080/"),cap);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
